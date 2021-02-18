1c21a80d113098440b9b6a517794f0fbea06a746
Dimas Surya <mdimas.umb@gmail.com>
2021-02-19 00:24:57 +0700

    Scraping website pkpu jadwal sholat jakarta dalam sebulan

---
+++ app.py
@@ -0,0 +1,30 @@
+import bs4
+import requests
+
+url = 'http://www.jadwalsholat.pkpu.or.id/monthly.php?id=83'
+contents = requests.get(url)
+
+response = bs4.BeautifulSoup(contents.text, "html.parser")
+data = response.find_all('tr', 'table_highlight')
+data = data[0]
+
+sholat = {}
+i = 0
+for d in data:
+    if i == 1:
+        sholat['shubuh'] = d.get_text()
+    elif i == 2:
+        sholat['zuhur'] = d.get_text()
+    elif i == 3:
+        sholat['ashar'] = d.get_text()
+    elif i == 4:
+        sholat['magrib'] = d.get_text()
+    elif i == 5:
+        sholat['isya'] = d.get_text()
+    i += 1
+print(sholat)
+print(sholat['shubuh'])
+
+
+
+
